44f5822fcad58e4b947e72a421ed4e3c77c9e413
Greg Opperman <gopperman@gmail.com>
2017-06-11 00:15:50 -0400

    Header micro-copy and basic styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,10 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <header className="header">
+          <h2 className="header__lede">Ok, so you need a car</h2>
+          <p className="header__subhed">You came to the right place. Let me just ask you a few quick questions:</p>
+      </header>
     )
   }
 }
