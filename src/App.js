a1214cb3d8ba57b612628783a9b10a5dc02568ac
Greg Opperman <gopperman@gmail.com>
2017-06-11 00:59:52 -0400

    Adding in some sample data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Header from './components/Header' 
 import './App.css';
+import data from './data/sample-response.json'
 
 class App extends Component {
-  render() {
-    return (
-      <Header />
-    )
-  }
+	constructor() {
+		super()
+
+		// Set initial state
+		this.state = { 
+			response: data, // 'Cached' API response
+			query: {
+				dest: 'BOS',
+				startDate: '01/20/2018',
+				endDate: '01/23/2018',
+				pickupTime: '10:00',
+				dropoffTime: '13:30'
+			}
+    	}
+	}
+	render() {
+		return (
+			<Header />
+		)
+	}
 }
 
 export default App;
